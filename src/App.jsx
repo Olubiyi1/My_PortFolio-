488c2bb4d7a2521fadca7f167da212248bc065e4
olubiyi babajide <olubiyibabajide@gmail.com>
2024-11-03 14:32:59 +0100

    Updating new branch

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const App = () => {
+   return ( 
+      <Main>
+         <h1>Hello world</h1>
+      </Main>
+    );
+}
+ 
+export default App;
+
+const Main = styled.div`
+   
+   
+`
